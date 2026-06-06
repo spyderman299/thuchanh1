f361f72b84693bbce5eda79f8eb28d5cab5dea64
spyderman299 <hoangcxdz@gmail.com>
2026-06-06 15:08:22 +0700

    Update index.jsx

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Divider, List, ListItem, ListItemText } from "@mui/material";
+import { List, ListItem, ListItemText, Divider } from "@mui/material";
 import { Link } from "react-router-dom";
 import fetchModel from "../../lib/fetchModelData";
 
-export default function UserList() {
+function UserList() {
   const [users, setUsers] = useState([]);
-
   useEffect(() => {
     fetchModel("/user/list")
-      .then((data) => setUsers(data))
-      .catch((error) => console.error("Error loading user list:", error));
+      .then((res) => setUsers(res.data))
+      .catch(console.error);
   }, []);
 
   return (
-    <List component="nav">
-      {users.map((user) => (
-        <React.Fragment key={user._id}>
-          <ListItem
-            button
-            component={Link}
-            to={`/users/${user._id}`}
-            style={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <ListItemText primary={`${user.first_name} ${user.last_name}`} />
+    <List>
+      {users.map((u) => (
+        <React.Fragment key={u._id}>
+          <ListItem button component={Link} to={`/users/${u._id}`}>
+            <ListItemText primary={`${u.first_name} ${u.last_name}`} />
           </ListItem>
           <Divider />
         </React.Fragment>
@@ -30,3 +24,4 @@ export default function UserList() {
     </List>
   );
 }
+export default UserList;
